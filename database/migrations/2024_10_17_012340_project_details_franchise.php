1ceb92088048a82c8c5882443b7a565a11518c89
JunelAmelon <junelamelon92@gmail.com>
2024-10-17 02:59:35 +0100

    first commit

---
+++ database/migrations/2024_10_17_012340_project_details_franchise.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('project_details_franchise', function (Blueprint $table) {
+    $table->id();
+    $table->unsignedBigInteger('user_id');
+    $table->string('business_name');
+    $table->string('character_comparison');
+    $table->text('activity_description');
+    $table->string('business_age');
+    $table->string('sector');
+    $table->text('key_products');
+    $table->text('proudest_achievement');
+    $table->string('customer_count');
+    $table->string('current_revenue');
+    $table->integer('scalability_score');
+    $table->text('franchise_motivation');
+    $table->text('current_locations');
+    $table->integer('franchise_target');
+    $table->text('franchise_reproducibility');
+    $table->text('key_resources');
+    $table->text('sector_growth');
+    $table->text('market_study');
+    $table->text('company_values');
+    $table->text('franchisee_requirements');
+    $table->text('competitors');
+    $table->text('location_data');
+    $table->text('franchisee_training');
+    $table->text('tools');
+    $table->string('business_song');
+    $table->text('success_plan');
+    $table->string('status');
+    $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2024_10_17_012352_project_details_marketplace_business.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+    Schema::create('project_details_marketplace_business', function (Blueprint $table) {
+    $table->id();
+    $table->unsignedBigInteger('user_id');
+    $table->string('business_name');
+    $table->text('attraction_reason');
+    $table->string('business_animal');
+    $table->integer('conviction_score');
+    $table->text('business_motivation');
+    $table->text('entrepreneurial_approach');
+    $table->text('personal_strengths');
+    $table->text('financing_plan');
+    $table->boolean('financing_help');
+    $table->decimal('budget', 10, 2);
+    $table->text('post_acquisition_action');
+    $table->text('business_strategy');
+    $table->string('status');
+    $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
